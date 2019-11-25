6fb8f1c8147729c835aa7919a27b4c72ce66d929
mpocar <55193915+mpocar@users.noreply.github.com>
2019-11-25 16:58:11 +0100

    Add files via upload

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingrediantes).map((igKey) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
       <li key={igKey}>
         <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
@@ -17,6 +18,12 @@ const orderSummary = (props) => {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
